4fa93e5bf355d2c4c7b0b7136db046ac921fe92a
HervalMata <hervalmataamparo@gmail.com>
2020-06-16 16:37:03 -0300

    Model de cores

---
+++ app/Models/Color.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Mnabialek\LaravelEloquentFilter\Traits\Filterable;
+
+class Color extends Model
+{
+    use SoftDeletes;
+    use Filterable;
+
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = ['color_name'];
+}
