ddde3e2bb38b5291016a16b6fcd6b1277af89102
HervalMata <hervalmataamparo@gmail.com>
2020-06-16 01:11:29 -0300

    Model de Produtos e relacionamento com categoria

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Mnabialek\LaravelEloquentFilter\Traits\Filterable;
 
@@ -14,4 +15,12 @@ class Category extends Model
     protected $dates = ['deleted_at'];
 
     protected $fillable = ['category_name', 'slug', 'active'];
+
+    /**
+     * @return HasMany
+     */
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = ['product_name', 'product_code', 'description', 'quantity', 'price', 'active', 'category_id'];
+
+    /**
+     * @return BelongsTo
+     */
+    public function categories()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
