a7fc8fa345254482a8c40d75861f392c4eb76606
HervalMata <hervalmataamparo@gmail.com>
2020-06-16 16:16:21 -0300

    Request de produtos

---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'product_name' => 'required|max:20',
+            'product_code' => 'required',
+            'description' => 'required',
+            'quantity' => 'required|integer|min:1',
+            'price' => 'required|numeric',
+            'active' => 'boolean',
+            'category_id' => 'required|exists:category,id'
+        ];
+    }
+}
