36ef8e0c5b5480448d2c51727eb83d71c607f689
HervalMata <hervalmataamparo@gmail.com>
2020-06-16 19:23:06 -0300

    Método index para relacionamento entre produtos e cores

--- app/Http/Controllers/Api/ProductColorController.php
+++ app/Http/Controllers/Api/ProductColorController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductColorResource;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductColorController extends Controller
 {
-    //
+    /**
+     * @param Product $product
+     * @return ProductColorResource
+     */
+    public function index(Product $product)
+    {
+        return new ProductColorResource($product);
+    }
 }
