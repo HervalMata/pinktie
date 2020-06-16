488a9b4183e30f8c7a743e543e8f695b9e2facb5
HervalMata <hervalmataamparo@gmail.com>
2020-06-16 16:07:09 -0300

    Método index de produtos

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,18 +3,29 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Filters\ProductFilter;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return AnonymousResourceCollection
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        /** @var ProductFilter $filter */
+        $filter = app(ProductFilter::class);
+        /** @var Builder $filterQuery */
+        $filterQuery = Product::with('category')->filtered($filter);
+        $products = $request->has('all') ? $filterQuery->get() : $filterQuery->paginate(10);
+        return ProductResource::collection($products);
     }
 
     /**
