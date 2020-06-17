d57952cd91647292345413250254b2002a44084b
HervalMata <hervalmataamparo@gmail.com>
2020-06-17 15:56:08 -0300

    Método para login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Tymon\JWTAuth\JWTAuth;
 
 class AuthController extends Controller
 {
-    //
+    use AuthenticatesUsers;
+
+    /**
+     * @param Request $request
+     * @return array|JsonResponse
+     * @throws ValidationException
+     */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+        $credentials = $this->credentials($request);
+        $token = JWTAuth::attempt($credentials);
+        return $token ? ['token' => $token] :
+            response()->json(
+                ['error' => Lang::get('auth.failed')]
+                , 400);
+    }
 }
