a47950a2331112f3261914b375d3c0ca20455032
HervalMata <hervalmataamparo@gmail.com>
2020-06-16 22:46:59 -0300

    Resource de material

--- app/Http/Resources/MaterialResource.php
+++ app/Http/Resources/MaterialResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ColorResource extends JsonResource
+class MaterialResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -15,7 +15,7 @@ class ColorResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'color_name' => $this->color_name,
+            'material_name' => $this->material_name,
             'slug' => $this->slug,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at
