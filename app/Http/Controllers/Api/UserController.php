0c9cca2002b5d6b144b6ddd7046c9bacfd306b3c
HervalMata <hervalmataamparo@gmail.com>
2020-06-17 12:51:09 -0300

    Método index de Usuários

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,18 +3,28 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Filters\UserFilter;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return AnonymousResourceCollection
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        /** @var UserFilter $filter */
+        $filter = app(UserFilter::class);
+        /** @var Builder $filterQuery */
+        $filterQuery = User::filtered($filter);
+        $users = $request->has('all') ? $filterQuery->get() : $filterQuery->paginate(10);
+        return UserResource::collection($users);
     }
 
     /**
