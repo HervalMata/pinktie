92fd3d6add4e543b4d5f2570f9f8acd32ab62c1d
HervalMata <hervalmataamparo@gmail.com>
2020-06-17 19:45:29 -0300

    Seeder para perfil de usuário

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -7,7 +7,21 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserProfile extends Model
 {
+    const BASE_PATH = 'app/public';
+    const DIR_USERS = 'users';
+    const DIR_USER_PHOTO = self::DIR_USERS . '/photos';
+    const USER_PHOTO_PATH = self::BASE_PATH . '/' . self::DIR_USER_PHOTO;
+
     protected $fillable = ['user_id', 'cidade_id', 'country', 'address', 'number', 'additional', 'province', 'cpf', 'photo', 'telefone', 'mobile'];
+
+    /**
+     * @return string
+     */
+    public static function photosPath()
+    {
+        $path = self::USER_PHOTO_PATH;
+        return storage_path($path);
+    }
 
     /**
      * @return BelongsTo
--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Models\User;
+use App\Models\UserProfile;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 
 class UsersSeeder extends Seeder
 {
@@ -12,9 +17,53 @@ class UsersSeeder extends Seeder
      */
     public function run()
     {
+        File::deleteDirectory(UserProfile::photosPath(), true);
         factory(User::class, 1)->create([
-            'email' => 'admin@user.com'
-        ]);
-        factory(User::class, 50)->create();
+            'email' => 'admin@user.com',
+            'role' => User::ROLE_SELLER
+        ])->each(function ($user) {
+            Model::reguard();
+            $user->updateWithProfile([
+                'photo' => $this->getAdminPhoto(),
+            ]);
+            Model::unguard();
+            $user->profile->save();
+        });
+        factory(User::class, 1)->create([
+            'email' => 'customer@user.com',
+            'role' => User::ROLE_CUSTOMER
+        ])->each(function ($user) {
+            Model::reguard();
+            $user->updateWithProfile([
+                'photo' => $this->getCustomerPhoto(),
+            ]);
+            Model::unguard();
+            $user->profile->save();
+        });
+        factory(User::class, 50)->create()->each(function ($user) {
+            $user->profile->save();
+        });
+    }
+
+    /**
+     * @return UploadedFile
+     */
+    private function getAdminPhoto()
+    {
+        return new UploadedFile(
+            storage_path('app/faker/users/1624_mod.jpg'),
+            Str::random(16) . 'jpg'
+        );
+    }
+
+    /**
+     * @return UploadedFile
+     */
+    private function getCustomerPhoto()
+    {
+        return new UploadedFile(
+            storage_path('app/faker/users/1624_mod.jpg'),
+            Str::random(16) . 'jpg'
+        );
     }
 }
