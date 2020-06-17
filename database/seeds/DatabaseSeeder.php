6b24bcc1ffd37bd8dc47698dba278aa0ab2ed6f7
HervalMata <hervalmataamparo@gmail.com>
2020-06-16 22:20:17 -0300

    Seeder de material

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CategoriesSeeder::class);
         $this->call(ProductsSeeder::class);
         $this->call(ColorSeeder::class);
+        $this->call(MaterialSeeder::class);
     }
 }
