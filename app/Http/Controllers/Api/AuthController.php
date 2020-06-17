8728150708dd3dc0204319888da911ef73e77c22
HervalMata <hervalmataamparo@gmail.com>
2020-06-17 15:58:50 -0300

    Método para logout

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 use Tymon\JWTAuth\JWTAuth;
 
@@ -27,5 +28,15 @@ class AuthController extends Controller
             response()->json(
                 ['error' => Lang::get('auth.failed')]
                 , 400);
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function logout(Request $request)
+    {
+        Auth::guard('api')->logout();
+        return response()->json([], 204);
     }
 }
