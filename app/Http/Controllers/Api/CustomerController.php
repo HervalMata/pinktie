ff01116ec5c0cd7a74008a849951e7286f40e081
HervalMata <hervalmataamparo@gmail.com>
2020-06-18 02:30:44 -0300

    Método update para cliente

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CustomerRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -21,5 +22,28 @@ class CustomerController extends Controller
         $data['photo'] = $data['photo']??null;
         $user = User::createCustomer($data);
         return [ 'token' => Auth::guard('api')->login($user)];
+    }
+
+    public function update(CustomerRequest $request)
+    {
+        $data = $request->all();
+        if($request->has('token')){
+            $token = $request->token;
+        }
+
+        if($request->has('remove_photo')){
+            $data["photo"] = null;
+
+        }
+
+        $user = Auth::guard('api')->user();
+        $user->updateWithProfile($data);
+
+        $resource = new UserResource($user);
+
+        return [
+            'user' => $resource->toArray($request),
+            'token' => Auth::guard('api')->login($user)
+        ];
     }
 }
