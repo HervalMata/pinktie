18fab5c3e5a6cb6f15706af41561e7f19964e87c
HervalMata <hervalmataamparo@gmail.com>
2020-06-22 20:30:16 -0300

    Métodos para cupom de descontos

--- app/Http/Controllers/Api/CouponController.php
+++ app/Http/Controllers/Api/CouponController.php
@@ -3,62 +3,93 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Filters\CouponFilter;
+use App\Http\Requests\CouponRequest;
+use App\Http\Resources\CouponResource;
+use App\Models\Coupon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class CouponController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return AnonymousResourceCollection
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        /** @var CouponFilter $filter */
+        $filter = app(CouponFilter::class);
+        /** @var Builder $filterQuery */
+        $filterQuery = Coupon::filtered($filter);
+        $coupons = $request->has('all') ? $filterQuery->get() : $filterQuery->paginate(5);
+        return CouponResource::collection($coupons);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return CouponResource
      */
-    public function store(Request $request)
+    public function store(CouponRequest $request)
     {
-        //
+        $coupon = Coupon::create($request->all());
+        $coupon->refresh();
+        return new CouponResource($coupon);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Coupon $coupon
+     * @return CouponResource
      */
-    public function show($id)
+    public function show(Coupon $coupon)
     {
-        //
+        return new CouponResource($coupon);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param CouponRequest $request
+     * @param Coupon $coupon
+     * @return CouponResource
      */
-    public function update(Request $request, $id)
+    public function update(CouponRequest $request, Coupon $coupon)
     {
-        //
+        $coupon->fill($request->all());
+        $coupon->save();
+        return new CouponResource($coupon);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Coupon $coupon
+     * @return JsonResponse
+     * @throws \Exception
      */
-    public function destroy($id)
+    public function destroy(Coupon $coupon)
     {
-        //
+        $coupon->delete();
+        return response()->json([], 204);
+    }
+
+    /**
+     * Restore the specified resource from storage.
+     *
+     * @param Coupon $coupon
+     * @return JsonResponse
+     * @throws \Exception
+     */
+    public function restore(Coupon $coupon)
+    {
+        $coupon->restore();
+        return response()->json([], 204);
     }
 }
