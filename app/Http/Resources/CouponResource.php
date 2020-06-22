9b3a368b3002c7d04d4b2b68cd70e831c40549ba
HervalMata <hervalmataamparo@gmail.com>
2020-06-22 20:21:55 -0300

    Resource para cupom de descontos

---
+++ app/Http/Resources/CouponResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CouponResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'coupon_code' => $this->coupon_code,
+            'amount' => $this->amount,
+            'amount_type' => $this->amount_type,
+            'expiry_date' => $this->expiry_date,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
