8e7e39167039501354254219b55b1aa06a9bc97e
HervalMata <hervalmataamparo@gmail.com>
2020-06-22 15:29:35 -0300

    Método para retonar todas as ordens de um usuário

--- app/Http/Controllers/Api/UserOrderController.php
+++ app/Http/Controllers/Api/UserOrderController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserOrderResource;
+use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\Auth;
 
 class UserOrderController extends Controller
 {
-    //
+
+    /**
+     * @return AnonymousResourceCollection
+     */
+    public function ordersByUser()
+    {
+        if (Auth::guard('api')->check()) {
+            $user_id = \auth('api')->user()->getKey();
+        }
+        $orders = Order::with('cities')->where('user_id', $user_id);
+        return UserOrderResource::collection($orders);
+    }
+
 }
