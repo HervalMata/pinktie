0ae668b95759552753630d86f72ae8ad508e578c
HervalMata <hervalmataamparo@gmail.com>
2020-06-22 14:29:46 -0300

    Método index de ordens

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\OrderResource;
+use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class OrderController extends Controller
 {
-    //
+    /**
+     * @return AnonymousResourceCollection
+     */
+    public function index()
+    {
+        $orders = Order::with('cities', 'users')->paginate(10);
+        return OrderResource::collection($orders);
+    }
 }
